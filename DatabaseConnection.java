f3a52e0a244220f10bf94013506fc5e6e6f68179
Prasoon Joshi <166649173+StackTracer420@users.noreply.github.com>
2026-04-27 16:24:52 +0530

    DB Connection class for BPA

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/bpa_platform_db";
     private static final String USER = "root";
-    private static final String PASSWORD = "mysql"; 
+    private static final String PASSWORD = "Prag@#123456"; 
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
